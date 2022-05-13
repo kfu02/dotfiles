7fe95cf79f31a45c8f2bf443c60895398a92f6c8
Kevin Fu <kevin.fu842@gmail.com>
2022-05-13 15:05:41 -0400

    try to be clever and fail

--- link.zsh
+++ link.zsh
@@ -2,16 +2,20 @@
 
 # simple script to sym-link dotfiles
 
+# after git clone, put this repo at this location
+# (duplicate in zshrc)
+DOTFILES_PATH="$HOME/.dotfiles"
+
 # link nvim dotfile
-ln -sf $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sf $DOTFILES_PATH/nvim/init.vim $HOME/.config/nvim/init.vim
 echo 'init.vim (nvim) linked'
 
 # link .p10k
-ln -sf $HOME/.dotfiles/zsh/config/p10k.zsh $HOME/.p10k.zsh
+ln -sf $DOTFILES_PATH/zsh/config/p10k.zsh $HOME/.p10k.zsh
 echo '.p10k linked'
 
 # link zshrc
-ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
+ln -sf $DOTFILES_PATH/zsh/zshrc $HOME/.zshrc
 echo '.zshrc linked'
 
 # restart shell to apply changes
