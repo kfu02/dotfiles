d187bebe1d577a8fde0665951e6c14bb8895e056
Kevin Fu <kevin.fu842@gmail.com>
2022-05-13 00:08:34 -0400

    add p10k dot to zsh/

--- link.zsh
+++ link.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 ln -sf $HOME/.dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
 echo 'init.vim (nvim) linked'
+ln -sf $HOME/.dotfiles/zsh/p10k.zsh $HOME/.p10k.zsh
+echo '.p10k linked'
 ln -sf $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 echo '.zshrc linked'
