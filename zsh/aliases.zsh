e85c6db4765119cf8027a243db9ce44d456fb383
Kevin Fu <kevin.fu842@gmail.com>
2022-05-16 13:50:49 -0400

    add ros sourcing for noetic

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -25,18 +25,10 @@ case `uname` in
   Darwin)
     # commands for OS X go here
     alias o='open'
-
-    # install neovim via Homebrew
-    # > brew install neovim
-    # (this binds directly to nvim)
   ;;
   Linux)
     # commands for Linux go here
     alias open='xdg-open'
     alias o='open'
-
-    # neovim 0.5 requires special appimage 
-    # (https://github.com/neovim/neovim/releases/tag/v0.5.0)
-    alias -g nvim='~/nvim.appimage'
   ;;
 esac
