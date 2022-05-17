7ea46fa43d2fe9fede4ca43274c7f18d6017eca5
Kevin Fu <kevin.fu842@gmail.com>
2022-05-17 10:52:12 -0400

    add clang-format

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -30,5 +30,12 @@ case `uname` in
     # commands for Linux go here
     alias open='xdg-open'
     alias o='open'
+
+    # GREENZIE
+    # clang format current file
+    alias cf='git add *.cpp *.h *.hpp *.ino && clang-format-9 -i -style=file $(comm -12 \
+    <(git diff origin/master --name-only | sort) \
+    <(find . -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.ino" \) | cut -c 3- | sort)) && \
+    git add *.cpp *.h *.hpp *.ino'
   ;;
 esac
