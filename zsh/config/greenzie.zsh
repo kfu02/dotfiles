77808c2b12615a81eeb5fee0cca875532a8e0b0b
Kevin Fu <kevin.fu842@gmail.com>
2022-05-20 17:15:15 -0400

    rm env vars

--- zsh/config/greenzie.zsh
+++ zsh/config/greenzie.zsh
@@ -18,11 +18,6 @@ alias cf='git add *.cpp *.h *.hpp *.ino && clang-format-9 -i -style=file $(comm 
 <(find . -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.ino" \) | cut -c 3- | sort)) && \
 git add *.cpp *.h *.hpp *.ino'
 
-# env variables
-export GREENZIE_PLATFORM=intrepid
-export GREENZIE_MODEL=p4
-export DECK_WIDTH_INCHES=61
-
 # properly set up name and email when running dch, put this in your .bashrc
 # from https://github.com/Greenzie/robotic-lawnmower/blob/noetic-devel/docs/Development.md#bumping-package-revisions
 alias dch="DEBEMAIL=\"$(git config --get user.email)\" DEBFULLNAME=\"$(git config --get user.name)\" dch"
