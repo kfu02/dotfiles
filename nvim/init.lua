e644c7c8a382b20604853d4183468614096e77dd
Kevin Fu <kfu38@gatech.edu>
2023-11-20 22:22:48 -0500

    fix the fold col and weird zR zM behavior for UFO

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,6 +46,16 @@ require("lazy").setup({
         "kevinhwang91/nvim-ufo",
         dependencies = { 'kevinhwang91/promise-async' },
         config = function()
+            -- ufo.nvim recommended
+            vim.o.foldcolumn = '0' -- not dynamic, I prefer not having fold guides at all to having extra
+            vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+            vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
             require("ufo").setup({})
         end,
     },
